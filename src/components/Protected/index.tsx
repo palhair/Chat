4fcaa7cb394de9285f56344b7135d33f7d4eb2aa
palhair <palad.ru@mail.ru>
2025-02-05 20:58:20 +0400

    hardcore url

--- src/components/Protected/index.tsx
+++ src/components/Protected/index.tsx
@@ -1,8 +1,7 @@
 import { Navigate, Outlet, useLocation } from 'react-router';
-// import { useLocation, Outlet } from 'react-router-dom';
 
 export const Protected = () => {
 	const location = useLocation();
 
-	return localStorage.getItem('user') ? <Outlet /> : <Navigate to='/auth' state={{ from: location }} replace />;
+	return localStorage.getItem('user') ? <Outlet /> : <Navigate to="/auth" state={{ from: location }} replace />;
 };
